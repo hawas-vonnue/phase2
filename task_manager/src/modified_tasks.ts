fbeca83cba54cf1567458f596832b7beff35eafe
Hawas backer <hawas.backer@vonnue.com>
2026-08-04 15:30:12 +0530

    feat: complete phase2 day3- Convert the task manager into a Node.js HTTP API without Express.
    - Convert the task manager into Node.js HTTP API.
    - Correct status codes for sucess, missing resources and bad request.

---
+++ task_manager/src/modified_tasks.ts
@@ -0,0 +1,89 @@
+import { readFile } from "node:fs/promises";
+import { writeFile } from "node:fs/promises";
+
+const filepath = "tasks.json";
+
+export type Task = {
+    id: number;
+    type: "completed" | "pending";
+    description: string;
+};
+
+export async function readFromFile(
+    path: string = filepath
+): Promise<Array<Task> | undefined> {
+    try {
+        const jsonData = await readFile(path, "utf-8");
+        const data = JSON.parse(jsonData);
+        if (data.tasks) return data.tasks;
+        else return [];
+    } catch (error) {
+        if (typeof error === "object" && error && "code" in error) {
+            if (error.code === "ENOENT") {
+                await writeToFile([]);
+                return [];
+            }
+        }
+    }
+}
+
+export async function writeToFile(arr: Array<Task>, path: string = filepath) {
+    try {
+        const data = { tasks: arr };
+        const jsonData = JSON.stringify(data);
+        await writeFile(path, jsonData, "utf-8");
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+export async function getTasks(path = filepath) {
+    const data = await readFromFile();
+
+    return data;
+}
+
+export async function getSpecificTask(id: number, path = filepath) {
+    const data = await readFromFile();
+    let task = data?.find((element) => element.id === id);
+    if (task !== undefined) {
+        return task;
+    } else return false;
+}
+
+export async function deleteTask(id: number) {
+    const data = await readFromFile();
+    const task = data?.find((task) => task.id === id);
+    if (!data || !task) {
+        return false;
+    }
+    const index = data.indexOf(task);
+    data.splice(index, 1);
+    if ((await writeToFile(data)) !== false) return task;
+    else return false;
+}
+
+export async function addToList(task: string) {
+    let data = await readFromFile();
+    if (!data) return;
+    let maxId = 0;
+    data?.map((element) => (maxId = Math.max(maxId, element.id)));
+    let id = maxId + 1;
+    data.push({ id: id, description: task, type: "pending" });
+    if ((await writeToFile(data)) !== false) return true;
+    else return false;
+}
+
+export async function updateTask(id: number, task: string) {
+    let data = await readFromFile();
+    if (!data || data.length === 0) return false;
+    const taskToUpdate = data.find((element) => element.id === id);
+    if (taskToUpdate === undefined) return false;
+    const index = data.indexOf(taskToUpdate);
+    taskToUpdate.description = task;
+    data.splice(index, 1, taskToUpdate);
+    await writeToFile(data);
+
+    return true;
+}
---
+++ task_manager/src/server.ts
@@ -0,0 +1,168 @@
+import http, { IncomingMessage, ServerResponse } from "node:http";
+import {
+    addToList,
+    deleteTask,
+    getSpecificTask,
+    getTasks,
+    updateTask,
+} from "./modified_tasks.js";
+
+export const server = http.createServer(async (request, response) => {
+    request.on("error", (err) => {
+        console.error(err);
+    });
+    const method = request.method;
+    const url = request.url;
+    if (method === "GET" && url === "/tasks") {
+        await getTasksResponder(request, response);
+    } else if (method === "GET" && url?.includes("/tasks/:")) {
+        await getTaskResponder(request, response, url);
+    } else if (method === "DELETE" && url?.includes("/tasks/:")) {
+        await deleteTaskResponder(request, response, url);
+    } else if (method === "POST") {
+        await postResponder(request, response);
+    } else if (method === "PATCH" && url?.includes("/tasks/:")) {
+        await patchResponder(request, response, url);
+    } else {
+        response.statusCode = 400;
+        response.end(JSON.stringify("Error: Bad request"));
+    }
+});
+if (process.env.NODE_ENV !== "test") server.listen(8080);
+
+async function getTasksResponder(
+    request: IncomingMessage,
+    response: ServerResponse
+) {
+    const tasks = await getTasks();
+    response.setHeader("content-type", "application/json");
+    response.statusCode = 200;
+    response.end(JSON.stringify(tasks));
+}
+
+async function getTaskResponder(
+    request: IncomingMessage,
+    response: ServerResponse,
+    url: string
+) {
+    const id = Number(url.split(":")[1]);
+    let task = await getSpecificTask(id);
+
+    response.setHeader("content-type", "application/json");
+    if (task === false) {
+        response.statusCode = 404;
+        response.end();
+    } else {
+        response.statusCode = 200;
+        response.end(JSON.stringify(task));
+    }
+}
+
+async function deleteTaskResponder(
+    request: IncomingMessage,
+    response: ServerResponse,
+    url: string
+) {
+    const id = Number(url.split(":")[1]);
+    let task = await deleteTask(id);
+
+    response.setHeader("content-type", "applicaton/json");
+    if (task === false) {
+        response.statusCode = 404;
+        response.end();
+    } else {
+        response.statusCode = 200;
+        response.end(JSON.stringify(task));
+    }
+}
+
+async function postResponder(
+    request: IncomingMessage,
+    response: ServerResponse
+) {
+    let bodyArray: Buffer[] = [];
+    let bodyJSON;
+    request
+        .on("data", (chunk) => {
+            bodyArray.push(chunk);
+        })
+        .on("end", async () => {
+            try {
+                bodyJSON = Buffer.concat(bodyArray).toString();
+                const body = JSON.parse(bodyJSON);
+                response.setHeader("content-type", "application/json");
+                let responseText;
+                if (
+                    body &&
+                    typeof body === "object" &&
+                    "description" in body &&
+                    typeof body.description === "string"
+                ) {
+                    let flag = await addToList(body.description);
+                    if (flag) {
+                        responseText = "Success";
+                        response.statusCode = 200;
+                    } else {
+                        responseText = "Failure";
+                        response.statusCode = 500;
+                    }
+                } else {
+                    responseText = "Failure due to bad request";
+                    response.statusCode = 400;
+                }
+                response.end(JSON.stringify(responseText));
+            } catch (error) {
+                response.setHeader("content-type", "application/json");
+                let responseText = "Failure due to bad request";
+                response.statusCode = 400;
+                response.end(responseText);
+            }
+        });
+}
+
+async function patchResponder(
+    request: IncomingMessage,
+    response: ServerResponse,
+    url: string
+) {
+    let id = Number(url.split(":")[1]);
+    let bodyArray: Buffer[] = [];
+    let bodyJSON;
+    response.setHeader("content-type", "application/json");
+
+    try {
+        request
+            .on("data", (chunk) => {
+                bodyArray.push(chunk);
+            })
+            .on("end", async () => {
+                bodyJSON = Buffer.concat(bodyArray).toString();
+                const body = JSON.parse(bodyJSON);
+                let responseText;
+                if (
+                    body &&
+                    typeof body === "object" &&
+                    "description" in body &&
+                    typeof body.description === "string"
+                ) {
+                    if (await updateTask(id, body.description)) {
+                        responseText = "updated Successfully";
+                        response.statusCode = 200;
+                        response.end(JSON.stringify(responseText));
+                    } else {
+                        responseText = "Couldnt find the task";
+                        response.statusCode = 404;
+                        response.end(JSON.stringify(responseText));
+                    }
+                } else {
+                    responseText = "Bad Request";
+                    response.statusCode = 400;
+                    response.end(JSON.stringify(responseText));
+                }
+            });
+    } catch (error) {
+        response.statusCode = 400;
+        let status = "Failure due to bad request";
+        response.end(JSON.stringify(status));
+    }
+}
