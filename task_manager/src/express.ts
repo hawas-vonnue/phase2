649f2e2753d4673617733678be8d2020ea907968
Hawas backer <hawas.backer@vonnue.com>
2026-08-05 09:40:53 +0530

    feat: complete phase2 day4- Migrate the Task API to Express.
    - Preserved all existing endpoints.
    - Added request logging and a health endpoint.
    - Added centralised not found and error handler.

---
+++ task_manager/src/express.ts
@@ -0,0 +1,148 @@
+import express, { type Response, type Request, NextFunction } from "express";
+import {
+    addToList,
+    deleteTask,
+    getSpecificTask,
+    getTasks,
+    updateTask,
+} from "./modified_tasks.js";
+
+const app = express();
+
+//parse json bodies
+app.use(express.json());
+
+//request logging
+app.use((req, res, next) => {
+    console.log("method:", req.method);
+    console.log("params", req.params);
+    console.log("body:", req.body);
+    next();
+});
+
+//setting header for all responses
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.set("Content-Type", "application/json");
+    next();
+});
+
+//health endpoint
+app.get("/health", (req: Request, res: Response) => {
+    res.status(200).send("Ok");
+});
+
+app.get("/tasks", getTasksResponder);
+app.get("/tasks/:id", getTaskResponder);
+app.post("/tasks", postResponder);
+app.patch("/tasks/:id", patchResponder);
+app.delete("/tasks/:id", deleteTaskResponder);
+
+app.use(notFoundHandler);
+
+//centralised errorHandler
+app.use(errorHandler);
+
+app.listen(8080);
+
+function notFoundHandler(req: Request, res: Response) {
+    res.status(404).json("Not Found");
+}
+
+function errorHandler(err: Error, req: Request, res: Response) {
+    res.status(500);
+    res.send(`error ${err}`);
+}
+
+async function getTasksResponder(
+    request: Request,
+    response: Response,
+    next: NextFunction
+) {
+    const tasks = await getTasks();
+    response.status(200).json(tasks);
+}
+
+async function getTaskResponder(
+    request: Request<{ id: string }>,
+    response: Response,
+    next: NextFunction
+) {
+    const id = Number(request.params.id);
+    let task = await getSpecificTask(id);
+
+    if (task === false) {
+        next();
+    } else {
+        response.status(200).json(task);
+    }
+}
+
+async function deleteTaskResponder(
+    request: Request,
+    response: Response,
+    next: NextFunction
+) {
+    const id = Number(request.params.id);
+    let task = await deleteTask(id);
+
+    response.setHeader("content-type", "applicaton/json");
+    if (task === false) {
+        next();
+    } else {
+        response.status(200).json(task);
+    }
+}
+
+async function postResponder(
+    request: Request,
+    response: Response,
+    next: NextFunction
+) {
+    try {
+        let body = request.body;
+        if (
+            body &&
+            typeof body === "object" &&
+            "description" in body &&
+            typeof body.description === "string"
+        ) {
+            let flag = await addToList(body.description);
+            if (flag) {
+                response.status(200).json("Success");
+            } else {
+                response.status(500).json("Failure");
+            }
+        } else {
+            response.status(400).json("Failure due to bad request");
+        }
+    } catch (error) {
+        next(error);
+    }
+}
+
+async function patchResponder(
+    request: Request,
+    response: Response,
+    next: NextFunction
+) {
+    let id = Number(request.params.id);
+    try {
+        const body = request.body;
+        if (
+            body &&
+            typeof body === "object" &&
+            "description" in body &&
+            typeof body.description === "string"
+        ) {
+            if (await updateTask(id, body.description)) {
+                response.status(200).json("updated Successfully");
+            } else {
+                next();
+            }
+        } else {
+            response.status(400).json("Bad Request");
+        }
+    } catch (error) {
+        next(error);
+    }
+}
