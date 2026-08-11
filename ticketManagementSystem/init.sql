e67986ec681187c7fac758f13a61e214636b539f
Hawas backer <hawas.backer@vonnue.com>
2026-08-11 14:09:04 +0530

    update: adjust init.sql with the changes made in feat/day6

--- ticketManagementSystem/init.sql
+++ ticketManagementSystem/init.sql
@@ -4,16 +4,16 @@ BEGIN;
 DROP TABLE IF EXISTS assignments, comments,status_history,tickets,categories,customers,users;
 
 
-CREATE TABLE customers(customerId INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,name VARCHAR(36) NOT NULL);
+CREATE TABLE customers(customerId INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,name VARCHAR(36) NOT NULL,email VARCHAR(36) NOT NULL UNIQUE);
 
 
 CREATE TABLE categories(categoryId int GENERATED ALWAYS AS IDENTITY PRIMARY KEY, category VARCHAR(36) NOT NULL);
 
 
-CREATE TABLE tickets(ticketId int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,title varchar(36),description TEXT,priority VARCHAR(10)CHECK(priority IN('High','Medium','Low')),status VARCHAR(10),customerId INT NOT NULL,categoryId INT NOT NULL,FOREIGN KEY(customerId) REFERENCES customers(customerId)ON DELETE CASCADE,FOREIGN KEY(categoryId) REFERENCES categories(categoryId)ON DELETE CASCADE);
+CREATE TABLE tickets(ticketId int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,title varchar(36),description TEXT,priority VARCHAR(10),status VARCHAR(10),customerId INT NOT NULL,categoryId INT NOT NULL,created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,FOREIGN KEY(customerId) REFERENCES customers(customerId),FOREIGN KEY(categoryId) REFERENCES categories(categoryId));
 
 
-CREATE TABLE users(userId INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,name VARCHAR(36) UNIQUE);
+CREATE TABLE users(userId INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,name VARCHAR(36),email VARCHAR(36) NOT NULL UNIQUE);
 
 
 CREATE TABLE comments(commentId INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,text TEXT,ticketId INT, FOREIGN KEY(ticketId) REFERENCES tickets(ticketId)ON DELETE CASCADE) ;
@@ -28,13 +28,13 @@ CREATE TABLE status_history(statusId INT GENERATED ALWAYS AS IDENTITY PRIMARY KE
 INSERT INTO categories(category) VALUES ('TECHNICAL'),('GENERAL') RETURNING *;
 
 
-INSERT INTO customers(name) VALUES ('Customer1'), ('Customer2'), ('Customer3') RETURNING *;
+INSERT INTO customers(name,email) VALUES ('Customer1','customer1@gmail.com'), ('Customer2','customer2@gmail.com'), ('Customer3','customer3@gmail.com') RETURNING *;
 
 
 INSERT INTO tickets(title,description,priority,status,customerId,categoryId) VALUES ('ticket 1','description of ticket 1','High','open',1,1) , ('ticket 2','description of ticket 2','Low','open',2,2) RETURNING *;
 
 
-INSERT INTO users(name) VALUES ('user 1'),('user 2'),('user 3') RETURNING *;
+INSERT INTO users(name,email) VALUES ('user 1','user1@gmail.com'),('user 2','user2@gmail.com'),('user 3','user3@gmail.com') RETURNING *;
 
 
 INSERT INTO comments(text,ticketId) VALUES ('this is comment on ticket with id 1',1),('this is second comment on ticket with id 1',1),('this is comment on ticket with id 2',2) RETURNING *;
